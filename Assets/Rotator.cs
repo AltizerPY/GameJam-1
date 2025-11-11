d6ba366c6133adfad8b97f4b086ff96903152a6d
AltizerPY <AltizerProgramming@gmail.com>
2025-11-11 11:31:11 -0500

    Setup
    Changed Camera to Orthographic, Cube aligned with Camera, cube spins, Panel used as placeholder for maps.

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     public float rotationSpeed = 50f; // Adjust this value in the Inspector for desired speed
-    public Vector3 rotationDirection = Vector3.up; // Default to rotating around the Y-axis
-
+    public Vector3 rotationDirectionY = Vector3.up; // Default to rotating around the Y-axis
     // Update is called once per frame
     void Update()
     {
         // Rotate the object around the specified axis at the given speed
-        transform.Rotate(rotationDirection * rotationSpeed * Time.deltaTime);
+        transform.Rotate(rotationDirectionY * rotationSpeed * Time.deltaTime);
     }
 }
