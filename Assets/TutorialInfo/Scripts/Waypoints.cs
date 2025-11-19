2003c0ee984391e4e0c1ddcb21b6d2046d48451a
jacobswoagerschool <jacomswoager001001001@gmail.com>
2025-11-19 11:07:49 -0500

    Revert "Merge branch 'main' into GameJam1-Jacob"
    This reverts commit 3a38fe23019283965d82fe6209f4d0d1ee61cc5e, reversing
    changes made to 88c163547004e395e3ac85d0f5ae2fa5f1237991.

--- Assets/TutorialInfo/Scripts/Waypoints.cs
+++ Assets/TutorialInfo/Scripts/Waypoints.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-public class Waypoints : MonoBehaviour
+public class Waypoint : MonoBehaviour
 {
-    public Transform[] WayPoints;
+    public Transform[] waypoints;
 }
